1f14ebc7b002a9e56489cbbf6d21aa61ca5e4235
romuloabbiati <68125906+romuloabbiati@users.noreply.github.com>
2022-07-25 05:30:58 +0100

    Add files via upload

---
+++ src/main/java/com/algaworks/algafood/api/v1/assembler/PermissaoModelAssembler.java
@@ -0,0 +1,49 @@
+package com.algaworks.algafood.api.v1.assembler;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algafood.api.v1.AlgaLinks;
+import com.algaworks.algafood.api.v1.controller.PermissaoController;
+import com.algaworks.algafood.api.v1.model.PermissaoModel;
+import com.algaworks.algafood.core.security.AlgaSecurity;
+import com.algaworks.algafood.domain.model.Permissao;
+
+@Component
+public class PermissaoModelAssembler 
+		extends RepresentationModelAssemblerSupport<Permissao, PermissaoModel>{
+
+	public PermissaoModelAssembler() {
+		super(PermissaoController.class, PermissaoModel.class);
+	}
+	
+	@Autowired
+	private ModelMapper modelMapper;
+	
+	@Autowired
+	private AlgaLinks algaLinks;
+	
+	@Autowired
+	private AlgaSecurity algaSecurity;
+	
+	@Override
+	public PermissaoModel toModel(Permissao permissao) {
+		PermissaoModel permissaoModel = modelMapper.map(permissao, PermissaoModel.class);
+		return permissaoModel;
+	}
+	
+	@Override
+	public CollectionModel<PermissaoModel> toCollectionModel(Iterable<? extends Permissao> entities) {
+		CollectionModel<PermissaoModel> collectionModel = super.toCollectionModel(entities);
+		
+		if(algaSecurity.podeConsultarUsuariosGruposPermissoes()) {
+			collectionModel.add(algaLinks.linkToPermissoes());
+		}
+		
+		return collectionModel;
+	}
+	
+}
---
+++ src/main/java/com/algaworks/algafood/api/v1/assembler/RestauranteBasicoModelAssembler.java
@@ -0,0 +1,61 @@
+package com.algaworks.algafood.api.v1.assembler;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algafood.api.v1.AlgaLinks;
+import com.algaworks.algafood.api.v1.controller.RestauranteController;
+import com.algaworks.algafood.api.v1.model.RestauranteBasicoModel;
+import com.algaworks.algafood.core.security.AlgaSecurity;
+import com.algaworks.algafood.domain.model.Restaurante;
+
+@Component
+public class RestauranteBasicoModelAssembler 
+		extends RepresentationModelAssemblerSupport<Restaurante, RestauranteBasicoModel> {
+
+	public RestauranteBasicoModelAssembler() {
+		super(RestauranteController.class, RestauranteBasicoModel.class);
+	}
+	
+	@Autowired
+	private ModelMapper modelMapper;
+	
+	@Autowired
+	private AlgaLinks algaLinks;
+	
+	@Autowired
+	private AlgaSecurity algaSecurity;
+	
+	@Override
+	public RestauranteBasicoModel toModel(Restaurante restaurante) {
+		RestauranteBasicoModel restauranteBasicoModel = createModelWithId(
+				restaurante.getId(), restaurante);
+		modelMapper.map(restaurante, restauranteBasicoModel);
+		
+		if(algaSecurity.podeConsultarRestaurantes()) {
+			restauranteBasicoModel.add(algaLinks.linkToRestaurantes("restaurantes"));
+		}
+		
+		if(algaSecurity.podeConsultarCozinhas()) {
+			restauranteBasicoModel.getCozinha().add(algaLinks
+					.linkToCozinha(restaurante.getCozinha().getId()));
+		}
+		
+		return restauranteBasicoModel;
+	}
+	
+	@Override
+	public CollectionModel<RestauranteBasicoModel> toCollectionModel(Iterable<? extends Restaurante> entities) {
+		 CollectionModel<RestauranteBasicoModel> collectionModel = super.toCollectionModel(entities);
+
+		 if(algaSecurity.podeConsultarRestaurantes()) {
+			 collectionModel.add(algaLinks.linkToRestaurantes());
+		 }
+		
+		return collectionModel;
+	}
+	
+}
