f6d17b79e49018e7510f3e504baa0352bb8b3d1a
romuloabbiati <68125906+romuloabbiati@users.noreply.github.com>
2022-07-25 05:31:54 +0100

    Add files via upload

---
+++ src/main/java/com/algaworks/algafood/api/v1/controller/GrupoPermissaoController.java
@@ -0,0 +1,89 @@
+package com.algaworks.algafood.api.v1.controller;
+
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algafood.api.v1.AlgaLinks;
+import com.algaworks.algafood.api.v1.assembler.PermissaoModelAssembler;
+import com.algaworks.algafood.api.v1.model.PermissaoModel;
+import com.algaworks.algafood.api.v1.openapi.controller.GrupoPermissaoControllerOpenApi;
+import com.algaworks.algafood.core.security.AlgaSecurity;
+import com.algaworks.algafood.core.security.CheckSecurity;
+import com.algaworks.algafood.domain.model.Grupo;
+import com.algaworks.algafood.domain.model.Permissao;
+import com.algaworks.algafood.domain.service.CadastroGrupoService;
+
+@RestController
+@RequestMapping(path = "/v1/grupos/{grupoId}/permissoes", 
+produces = MediaType.APPLICATION_JSON_VALUE)
+public class GrupoPermissaoController implements GrupoPermissaoControllerOpenApi{
+
+	@Autowired
+	private CadastroGrupoService cadastroGrupo;
+	
+	@Autowired
+	private PermissaoModelAssembler permissaoModelAssembler;
+	
+	@Autowired
+	private AlgaLinks algaLinks;
+	
+	@Autowired
+	private AlgaSecurity algaSecurity;
+	
+	@CheckSecurity.UsuariosGruposPermissoes.PodeConsultar
+	@GetMapping
+	public CollectionModel<PermissaoModel> listar(@PathVariable Long grupoId) {
+		Grupo grupo = cadastroGrupo.buscarOuFalhar(grupoId);
+		
+		Set<Permissao> permissoes = grupo.getPermissoes();
+		
+		CollectionModel<PermissaoModel> permissoesCollectionModel = permissaoModelAssembler
+				.toCollectionModel(permissoes)
+				.removeLinks();
+		
+		permissoesCollectionModel.add(algaLinks.linkToGrupoPermissoes(grupoId));
+		
+		if(algaSecurity.podeEditarUsuariosGruposPermissoes()) {
+			permissoesCollectionModel.add(algaLinks.linkToGrupoPermissaoAssociacao(grupoId, "associar"));
+			
+			permissoesCollectionModel.getContent().forEach(permissaoModel -> {
+				permissaoModel.add(algaLinks.linkToGrupoPermissaoDesassociacao(
+						grupoId, permissaoModel.getId(), "desassociar"));
+			});
+		}
+		
+		return permissoesCollectionModel;
+	}
+	
+	@CheckSecurity.UsuariosGruposPermissoes.PodeEditar
+	@PutMapping("/{permissaoId}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public ResponseEntity<Void> associarPermissao(@PathVariable Long grupoId, @PathVariable Long permissaoId) {
+		cadastroGrupo.associarPermissao(grupoId, permissaoId);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@CheckSecurity.UsuariosGruposPermissoes.PodeEditar
+	@DeleteMapping("/{permissaoId}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public ResponseEntity<Void> desassociarPermissao(@PathVariable Long grupoId, @PathVariable Long permissaoId) {
+		cadastroGrupo.desassociarPermissao(grupoId, permissaoId);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	
+}
